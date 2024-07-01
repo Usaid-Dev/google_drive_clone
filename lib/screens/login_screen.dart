6afffaa2cc49ab4b61c92c99ff78462387be17e2
Usaid Asif <usaidasif12@gmail.com>
2024-07-01 19:36:36 +0500

    Ui created of login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,6 +10,78 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset('images/gdrive.png'),
+          ),
+          const Text(
+            'Google Drive Clone',
+            style: TextStyle(
+              fontSize: 35,
+            ),
+          ),
+          const SizedBox(
+            height: 70,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 50, right: 50),
+            child: MaterialButton(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              elevation: 10,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://cdn-icons-png.flaticon.com/512/2991/2991148.png'),
+                          fit: BoxFit.cover),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  Text(
+                    "Sign In with Google",
+                    style: TextStyle(
+                      color: Colors.indigo[900],
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              onPressed: () async {},
+            ),
+          ),
+          const SizedBox(
+            height: 90,
+          ),
+          const Text(
+            'Made By',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Usaid Asif',
+                style: TextStyle(
+                  fontSize: 35,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
